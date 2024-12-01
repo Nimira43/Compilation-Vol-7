2869323bf8f1047c2ef0ecd28c1a5cdb258f7094
Nimira43 <n.rabone72@aol.com>
2024-12-01 11:08:12 +0000

    JavaScript code modernised and updated. Mini project completed

--- 16-Calculator/main.js
+++ 16-Calculator/main.js
@@ -1,6 +1,60 @@
 const opts = ['*', '/', '+', '-', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.'];
 const spec = ['*', '/', '+', '-'];
 
+const cOutput = (v) => {
+  const output = document.getElementById('output');
+  output.style.border = `${v} 1px solid`;
+  output.style.color = v;
+};
+
+const evalOutput = () => {
+  const output = document.getElementById('output');
+  cOutput('black');
+  if (output.value === "") {
+    cOutput('red');
+  } else if (eva) {
+    cOutput('red');
+  } else {
+    output.value = eval(output.value);
+  }
+  dec = output.value.includes('.');
+};
+
+const clrOutput = () => {
+  const output = document.getElementById('output');
+  cOutput('black');
+  output.value = "";
+};
+
+const btnMaker = (txt, myFunction) => {
+  let btn = document.createElement('button');
+  btn.classList.add('calculator-button');
+  btn.val = txt;
+  btn.textContent = txt;
+  btn.addEventListener('click', myFunction);
+  const main = document.getElementById('main');
+  main.appendChild(btn);
+};
+
+const addOutput = (e) => {
+  const output = document.getElementById('output');
+  cOutput('black');
+  let char = e.target.val;
+  if (char === '.') {
+    if (dec) {
+      char = '';
+      cOutput('red');
+    } else {
+      dec = true;
+    }
+  }
+  eva = spec.includes(char);
+  if (eva) {
+    dec = false;
+  }
+  output.value += char;
+};
+
 const init = () => {
   document.title = "Calculator";
   let dec = false;
